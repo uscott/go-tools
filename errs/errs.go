ced0b958a284293b079e83c421bccd25008fa5e6
US <uriel.scott@protonmail.com>
2020-07-12 07:16:58 -0500

    Add ErrNotConnected

--- errs/errs.go
+++ errs/errs.go
@@ -6,12 +6,13 @@ import (
 
 // Error values
 var (
-	ErrCapacity    = errors.New("insufficient capacity")
-	ErrConversion  = errors.New("type conversion error")
-	ErrDivByZero   = errors.New("divide by zero")
-	ErrDimMismatch = errors.New("dimension mismatch")
-	ErrInf         = errors.New("+/-Infinity")
-	ErrKey         = errors.New("map key error")
-	ErrNaN         = errors.New("NaN")
-	ErrNilPtr      = errors.New("nil pointer")
+	ErrCapacity     = errors.New("insufficient capacity")
+	ErrConversion   = errors.New("type conversion error")
+	ErrDivByZero    = errors.New("divide by zero")
+	ErrDimMismatch  = errors.New("dimension mismatch")
+	ErrInf          = errors.New("+/-Infinity")
+	ErrKey          = errors.New("map key error")
+	ErrNaN          = errors.New("NaN")
+	ErrNilPtr       = errors.New("nil pointer")
+	ErrNotConnected = errors.New("not connected")
 )
