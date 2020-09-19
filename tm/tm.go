ce3f66bcd16123105a08ef8596a54a78a8d96f19
US <uriel.scott@protonmail.com>
2020-09-18 22:56:48 -0500

    Add Format2

--- tm/tm.go
+++ tm/tm.go
@@ -7,7 +7,8 @@ import (
 
 const oneMillion = 1000 * 1000
 
-// Format0 returns the time argument formatted in a certain way
+// Format0 returns the time argument formatted first in RFC3339
+// then with "Z" removed, then "T" replaced with white space
 func Format0(t time.Time) string {
 	s := strings.ReplaceAll(t.Format(time.RFC3339), "Z", "")
 	return strings.ReplaceAll(s, "T", " ")
@@ -16,6 +17,11 @@ func Format0(t time.Time) string {
 // Format1 returns the format returned by Format0 with ':' removed
 func Format1(t time.Time) string {
 	return strings.ReplaceAll(Format0(t), ":", "")
+}
+
+// Format2 returns the date as YYYY-MM-DD
+func Format2(t time.Time) string {
+	return t.Format(time.RFC3339)[:10]
 }
 
 func GetTmStmp() float64 { // Timestamp in milliseconds
