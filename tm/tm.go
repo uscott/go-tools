2c975c03c0cfb46c1a9b9d1f0af9ee6abc3aa8d2
US <uriel.scott@protonmail.com>
2020-12-09 18:37:19 -0600

    Add some more comments

--- tm/tm.go
+++ tm/tm.go
@@ -12,17 +12,20 @@ var TimeZero = time.Date(1970, 1, 1, 0, 0, 0, 0, time.FixedZone("utc", 0))
 
 // Format0 returns the time argument formatted first in RFC3339
 // then with "Z" removed, then "T" replaced with white space
+// So YYYY-MM-DDThh:mm:ddZ => YYYY-MM-DD hh:mm:dd
 func Format0(t time.Time) string {
 	s := strings.ReplaceAll(t.Format(time.RFC3339), "Z", "")
 	return strings.ReplaceAll(s, "T", " ")
 }
 
 // Format1 returns the format returned by Format0 with ':' removed
+// YYYY-MM-DDThh:mm:ddZ => YYYY-MM-DD hhmmdd
 func Format1(t time.Time) string {
 	return strings.ReplaceAll(Format0(t), ":", "")
 }
 
 // Format2 returns the date as YYYY-MM-DD
+// YYYY-MM-DDThh:mm:ddZ => YYYY-MM-DD
 func Format2(t time.Time) string {
 	return t.Format(time.RFC3339)[:10]
 }
