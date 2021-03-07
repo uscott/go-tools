0c0a5f4a6c1dd306c5f9e81600cafdd8136449d0
US <uriel.scott@protonmail.com>
2021-03-06 19:19:04 -0600

    Write Recursive Elem

--- ref/ref.go
+++ ref/ref.go
@@ -3,3 +3,17 @@ package ref
 import (
 	"reflect"
 )
+
+func RecursiveElem(v reflect.Value) reflect.Value {
+
+	for {
+		kind := v.Kind()
+		if kind == reflect.Ptr || kind == reflect.Interface {
+			v = v.Elem()
+		} else {
+			break
+		}
+	}
+
+	return v
+}
